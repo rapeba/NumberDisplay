4afc18d6a1a26ab8c6f72b605a25a04e4fe26a31
Your Name <raulperezbaza@gmail.com>
2015-11-13 11:13:57 +0100

    Constructor que recibe por parámetro el límite del display y que fija el valor actual del display a 0.

---
+++ NumberDisplay.java
@@ -0,0 +1,35 @@
+
+/**
+ * Write a description of class NumberDisplay here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class NumberDisplay
+{
+    // instance variables - replace the example below with your own
+    private int limiteDisplay;
+    private int valorActual;
+
+    /**
+     * Constructor que recibe por parámetro el límite del display y que fija el valor actual del display a 0.
+     */
+   public NumberDisplay (int limiteDisplay)
+    {
+        // initialise instance variables
+        limiteDisplay = 0;
+     
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void setvalue()
+    {
+        // put your code here
+       
+    }
+}
